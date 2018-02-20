6ba430071ca84fa85e6c76197ee710a46c7fb563
Kristaps Ozols <ozols1992@hotmail.com>
2018-02-20 11:26:06 +0100

    draw kitchen shape

--- js/shape_drawer.js
+++ js/shape_drawer.js
@@ -1,28 +1,31 @@
-var canvas = new fabric.Canvas('can', { selection: false });
+$(function() {
+  var letsdraw = false;
+  var theCanvas = document.getElementById('can');
+  var ctx = theCanvas.getContext('2d');
+  var canvasOffset = $('#can').offset();
+  var lastpoints = {
+    "x": 0,
+    "y": 0
+  };
 
-var line, isDown;
-
-canvas.on('mouse:down', function(o){
-  isDown = true;
-  var pointer = canvas.getPointer(o.e);
-  var points = [ pointer.x, pointer.y, pointer.x, pointer.y ];
-  line = new fabric.Line(points, {
-    strokeWidth: 5,
-    fill: 'red',
-    stroke: 'red',
-    originX: 'center',
-    originY: 'center'
+  $('#can').mousemove(function(e) {
+    if (letsdraw === true) {
+      lastpoints.x = e.pageX;
+      lastpoints.y = e.pageY;
+    }
   });
-  canvas.add(line);
-});
 
-canvas.on('mouse:move', function(o){
-  if (!isDown) return;
-  var pointer = canvas.getPointer(o.e);
-  line.set({ x2: pointer.x, y2: pointer.y });
-  canvas.renderAll();
-});
+  $('#can').mousedown(function(e) {
+    letsdraw = true;
+    ctx.strokeStyle = 'lightgrey';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
+  });
 
-canvas.on('mouse:up', function(o){
-  isDown = false;
+  $('#can').mouseup(function(e) {
+    ctx.lineTo(lastpoints.x - canvasOffset.left, lastpoints.y - canvasOffset.top);
+    ctx.stroke();
+    letsdraw = false;
+  });
 });
